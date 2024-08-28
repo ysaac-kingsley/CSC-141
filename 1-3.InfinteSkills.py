690ae2333479d1e6e273ce3a8be23d8ccc775292
Ysaac-Kingsley <77698691+ysaac-kingsley@users.noreply.github.com>
2024-08-28 15:17:43 -0400

    Add files via upload

---
+++ 1-3.InfinteSkills.py
@@ -0,0 +1,7 @@
+'''
+Skills I want to have by the end of this course
+    1. I want to be able to make a keylogger using python
+    2. Chatbot that shows realtime stock info and news.
+    3. Voice activated game movement or smart home system.
+
+'''
