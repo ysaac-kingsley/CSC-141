ee8e010ee878312f2c4b636c429f8c192c4c3ed5
Ysaac-Kingsley <77698691+ysaac-kingsley@users.noreply.github.com>
2024-10-17 12:09:48 -0400

    Add files via upload

---
+++ Ch_5/05_011_Ordinal_Numbers.py
@@ -0,0 +1,11 @@
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for num in nums:
+    if num == 1:
+        print("1st")
+    elif num == 2:
+        print("2nd")
+    elif num == 3:
+        print("3rd")
+    else:
+        print(f'{num}th')
---
+++ Ch_5/05_03_Alien_Colors_1.py
@@ -0,0 +1,10 @@
+alien_color = 'yellow'
+
+if alien_color  == 'green':
+    print("You just earned 5 points!")
+elif alien_color == 'yellow':
+    print("You just earned 3 points!")
+else:
+    alien_color = "yellow"
+
+#this loop test's both failure and succession
